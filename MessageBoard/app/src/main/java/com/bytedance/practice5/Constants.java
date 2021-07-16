d50f72253973ff58e6cf7a08d53fd96f56815378
沈文豪 <shenwenhao@shenwenhaodeMacBook-Pro.local>
2021-07-16 19:25:15 +0800

    Homework5

---
+++ MessageBoard/app/src/main/java/com/bytedance/practice5/Constants.java
@@ -0,0 +1,13 @@
+package com.bytedance.practice5;
+
+public class Constants {
+    public static final String BASE_URL = "https://api-android-camp.bytedance.com/zju/invoke/";
+    public static final String MSG_PATH = "messages";
+    public static final String token = "U0pUVS1ieXRlZGFuY2UtYW5kcm9pZA==";
+    //TODO 1这里写上自己的学号和名字
+    public static final String STUDENT_ID = "3190102351";
+    public static final String USER_NAME = "沈文豪";
+    public static final String BOUNDARY = "156383456";
+    public static final String PRE_FIX = ("\r\n--" + BOUNDARY + "\r\n");
+    public static final String END_FIX = ("\r\n--" + BOUNDARY + "--\r\n");
+}
